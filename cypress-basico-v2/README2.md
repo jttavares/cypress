f42c6047757b5e233fa8854ab890711591767ab1
jttavares <32249679+jttavares@users.noreply.github.com>
2022-07-15 01:14:21 -0300

    Create cypress project

---
+++ cypress-basico-v2/README2.md
@@ -0,0 +1,32 @@
+    # cypress-basico-v2
+    Sample project for the basic course of the Talking About Testing school.
+
+    ## Pre-requirements
+    It is required to have Node.js and npm installed to run this project.
+    > I used versions `v16.13.3` and ´8.8.2´ of node and npm, respectively. I suggest you uswe the same or later versions.
+    
+    ## Installetion
+    Run npm install.
+
+    ## Tests
+    Yu can run the test simulating desktop or mobile version.
+
+    ### Desktop
+
+    Run `npm test` or `npm t` to ruun the test in headless mode.
+
+    Or run `npm run cy:open` to open in interactive mode.
+
+    ### Mobile
+    Run `npm test:mobile`  to run the test in headless mode.
+
+    Or run `npm run cy:open:mobile` to open in interactive mode.
+
+    ## Support for this project
+    If you want to support this project, leve a star
+
+    ---
+    This project was created by [José Tavares](https://scottx.net)
+
+
+
---
+++ cypress-basico-v2/cypress/e2e/integration/CAC-TAT.spec.cy.js
@@ -0,0 +1,132 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe("example to-do app", () => {
+    beforeEach(() => {
+      // Cypress starts out with a blank slate for each test
+      // so we must tell it to visit our website with the `cy.visit()` command.
+      // Since we want to visit the same URL at the start of all our tests,
+      // we include it in our beforeEach function so that it runs before each test
+      //
+      cy.visit("./src/index.html");
+    });
+  
+    it('acessa a pagina da politica removendo o target e entao clicando no link',()=>{
+       cy.get('#privacy a')
+        .invoke('removeAttr','target')
+        .click()
+  
+        cy.contains('Talking About Testing').should('be.visible')
+    })
+    it('verifica que a politica de privacidade abre em outra aba sem a necessidade de um clique',()=>{
+       cy.get('#privacy a').should('have.attr', 'target','_blank');
+    })
+  
+    it("seleciona um arquivo utilizando uma fixture para  qual foi dada um alias", () => {
+      it("seleciona um arquivo simulando drag an drop", () => {
+        cy.fixture("example.json").as("sampleFile");
+        cy.get('input[type="file"]#file-upload') //OU cy.get('input[type="file"]')
+          .selectFile("@sampleFile", { action: "drag-drop" })
+          .should((input) => {
+            console.log(input);
+            expect(input[0].files[0].name).to.equal("example.json");
+          });
+      });
+    });
+  
+    it("seleciona um arquivo simulando drag an drop", () => {
+      cy.get('input[type="file"]#file-upload') //OU cy.get('input[type="file"]')
+        .should("not.have.value")
+        .selectFile("./cypress/e2e/fixtures/example.json", { action: "drag-drop" })
+        .should((input) => {
+          console.log(input);
+          expect(input[0].files[0].name).to.equal("example.json");
+        });
+    });
+  
+    it("seleciona um arquivo da pasta feature", () => {
+      cy.get('input[type="file"]#file-upload') //OU cy.get('input[type="file"]')
+        .should("not.have.value")
+        .selectFile("./cypress/fixtures/example.json")
+        .should((input) => {
+          console.log(input);
+          expect(input[0].files[0].name).to.equal("example.json");
+        });
+    });
+  
+    it("exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário", () => {
+      cy.get("#firstName").type("Jose");
+      cy.get("#lastName").type("Tavares");
+      cy.get("#email").type("jttavaresg@gmail.com");
+      cy.get("#phone-checkbox").check();
+      cy.get("#open-text-area").type("Teste");
+      //cy.get('button[type="submit"]').click();
+      cy.contains("button", "Enviar").click();
+  
+      cy.get(".error").should("be.visible");
+    });
+  
+    it("marca ambo os checkbox, depois desmarca o último", () => {
+      cy.get('input[type="checkbox"]')
+        .check()
+        .last()
+        .uncheck()
+        .should("not.be.checked");
+    });
+  
+    it("marca cada tipo de atendimento", () => {
+      cy.get('input[type="radio"]')
+        .should("have.length", 3)
+        .each(($radio) => {
+          cy.wrap($radio).check();
+          cy.wrap($radio).should("be.checked");
+        });
+    });
+  
+    it('marca o tipo de atendimento "Feedback"', () => {
+      cy.get('input[type="radio"][value="feedback"]')
+        .check()
+        .should("have.value", "feedback");
+    });
+  
+    it("Selecionar produto por seu texto", () => {
+      cy.get("#product").select("YouTube").should("have.value", "youtube");
+    });
+  
+    it("Selecionar produto por seu valor", () => {
+      cy.get("#product").select("mentoria").should("have.value", "mentoria");
+    });
+  
+    it("Selecionar produto por seu index", () => {
+      cy.get("#product").select(1).should("have.value", "blog");
+    });
+  
+    it("Envia o formulário com sucesso usando comandos customizados", () => {
+      cy.fillMandatoryFieldsAndSubmit();
+      cy.get(".success").should("be.visible");
+    });
+  
+    it("Exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios", () => {
+      /*
+      cy.get('#firstName')
+      cy.get('#lastName')
+      cy.get('#email')
+      cy.get('#open-text-area')
+      */
+      cy.get('button[type="submit"]').click();
+      cy.get(".error").should("be.visible");
+    });
+  
+   
+
+})
